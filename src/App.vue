af0b628f534b1bc6e6319181981e81b6bf50c944
Kamen Bundev <bundyo@gmail.com>
2018-08-15 18:58:32 +0300

    fix: missing content

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,26 @@
 <template>
     <div id="app">
-        <list :source="source"/>
+        <fusion-list :source="source"/>
     </div>
 </template>
 
 <script>
+    import Vue from "vue";
     import list from "./components/list.vue";
+
+    Vue.component(list.name, list);
 
     export default {
         name: "app",
-        components: {
-            list
-        },
-            data() {
-                return {
-                    source: [
-                        { text: "1" }, { text: "2" }, { text: "3" }, { text: "4" }, { text: "5" }, { text: "6" },
-                        { text: "7" }, { text: "8" }, { text: "9" }, { text: "10" }, { text: "11" }, { text: "12" },
-                        { text: "13" }, { text: "14" }, { text: "15" }, { text: "16" }, { text: "17" }, { text: "18" }
-                    ]
-                };
-            }
+        data() {
+            return {
+                source: [
+                    { text: "1" }, { text: "2" }, { text: "3" }, { text: "4" }, { text: "5" }, { text: "6" },
+                    { text: "7" }, { text: "8" }, { text: "9" }, { text: "10" }, { text: "11" }, { text: "12" },
+                    { text: "13" }, { text: "14" }, { text: "15" }, { text: "16" }, { text: "17" }, { text: "18" }
+                ]
+            };
+        }
     }
 </script>
 
--- src/components/list.vue
+++ src/components/list.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="ab-list-container" ref="container">
-        <div class="ab-list-sizer"></div>
+    <div class="ab-list-content" ref="content">
+        <div class="ab-list-sizer" ref="sizer"></div>
         <div v-for="(row, index) in view" :key="index" class="ab-list-row">
-            {{ row[row[textField]] }}
+            {{ row[textField] }}
         </div>
     </div>
 </template>
@@ -18,79 +18,123 @@
 
     export default {
         name: "fusion-list",
-        options: {
-            props: {
-                textField: {
-                    type: String,
-                    default: "text"
-                },
-                source: {
-                    type: Array,
-                    default: () => []
-                }
+        props: {
+            textField: {
+                type: String,
+                default: "text"
             },
-
-            data() {
-                return {
-                    index: 0,
-                    pageSize: 10,
-                    textField: "text"
-                };
+            pageSize: {
+                type: Number,
+                default: 10
             },
-
-            methods: {
-                adjustHeight() {
-                    if (!this.$el) {
-                        return;
-                    }
-
-                    const firstChild = this.$el.firstChild;
-
-                    if (firstChild) {
-                        firstChild.style.height = `${this.source.length * parseFloat(firstChild.nextElementSibling.offsetHeight)}px`;
-                    }
-                },
-
-                instersectionCallback(entries) {
-                    entries.forEach(entry => {
-                        this.index = index(entry) - this.pageSize || 0;
-                    });
-                }
-            },
-
-            computed: {
-                view() {
-                    this.adjustHeight();
-
-                    return this.source.slice(this.index, this.pageSize);
-                }
-            },
-
-            created() {
-                this.observer = new IntersectionObserver(this.instersectionCallback.bind(this), {
-                    root: this.$refs.container,
-                    rootMargin: "0px",
-                    threshold: 1
-                });
-            },
-
-            mounted() {
-                this.adjustHeight();
-
-                if (this.$el.children) {
-                    Array.from(this.$el.children).forEach(child => {
-                        this.observer.observe(child);
-                    });
-                }
-            },
-
-            unmounted() {
-                if (this.$el.children) {
-                    Array.from(this.$el.children).forEach(child => {
-                        this.observer.unobserve(child);
-                    });
-                }
+            source: {
+                type: Array,
+                default: () => []
             }
+        },
+
+        data() {
+            return {
+                index: 0,
+                content: null,
+                sizer: null,
+                firstRow: null,
+                lastRow: null
+            };
+        },
+
+        methods: {
+            getHeight() {
+                const content = this.content;
+                const children = Array.from(content.children);
+
+                if (!children[1]) {
+                    return 0;
+                }
+
+                this.firstRow = children[1];
+                this.lastRow = children[children.length - 1];
+
+                this.firstRowRect = this.firstRow.getBoundingClientRect();
+                this.lastRowRect = this.lastRow.getBoundingClientRect();
+
+                if (this.sizer && this.firstRow) {
+                    return (this.lastRowRect.top - this.firstRowRect.top + this.lastRowRect.height) / this.view.length;
+                }
+
+                return 0;
+            },
+
+            adjustDimensions(height) {
+                if (!this.$el) {
+                    return;
+                }
+
+                if (!this.source.length) {
+                    this.index = 0;
+                }
+
+                height = height || this.getHeight();
+
+                if (this.firstRow) {
+                    this.firstRow.style.marginTop = `${this.index * height}px`;
+                }
+            },
+
+            recalcView() {
+                const height = this.getHeight();
+
+                this.sizer.style.height = `${this.source.length * height}px`;
+
+                this.adjustDimensions(height);
+            },
+
+            instersectionCallback(entries) {
+                entries.forEach(entry => {
+                    this.index = index(entry) - this.pageSize || 0;
+                });
+            }
+        },
+
+        computed: {
+            view() {
+                return this.source.slice(this.index, this.pageSize);
+            }
+        },
+
+        watch: {
+            source() {
+                this.$nextTick(this.recalcView.bind(this));
+            }
+        },
+
+        created() {
+            this.observer = new IntersectionObserver(this.instersectionCallback.bind(this), {
+                root: this.$refs.content,
+                rootMargin: "0px",
+                threshold: 1
+            });
+        },
+
+        mounted() {
+            this.content = this.$refs.content;
+            this.sizer = this.$refs.sizer;
+
+            this.recalcView();
+
+            //if (this.content.children) {
+            //    Array.from(this.content.children).forEach(child => {
+            //        this.observer.observe(child);
+            //    });
+            //}
+        },
+
+        unmounted() {
+            //if (this.content.children) {
+            //    Array.from(this.content.children).forEach(child => {
+            //        this.observer.unobserve(child);
+            //    });
+            //}
         }
     }
 </script>
@@ -102,7 +146,7 @@
         border-width: 1px 0;
     }
 
-    .ab-list-container {
+    .ab-list-content {
         position: relative;
         height: 200px;
         overflow: hidden;
