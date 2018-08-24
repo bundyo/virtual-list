4f9d4adcc2242c0d890df7fa65c2d98270f01ae4
Kamen Bundev <bundyo@gmail.com>
2018-08-24 08:00:09 +0300

    IntersectionObserver fail point

--- src/components/list.vue
+++ src/components/list.vue
@@ -1,29 +1,19 @@
 <template>
-    <div class="fs-list" tabindex="-1">
-        <div ref="content" class="fs-list-content">
-            <component :is="rowComponent" v-for="(item, key) in view" :key="key + index" :item="item" class="fs-list-row"
-                       :index="key + index" :style="{ marginTop: key === 0 ? `${firstMargin}px` : 0 }"
-                       v-notify-mount @mounted="$nextTick(() => observer.observe($event))">
+    <div class="ab-list">
+        <div class="ab-list-content" ref="content" tabindex="-1">
+            <div class="ab-list-sizer" ref="sizer" :style="{ height: sizerHeight }"></div>
+            <component :is="rowComponent" v-for="(item, key) in view" :key="key + index" :item="item" :index="key + index" class="ab-list-row"
+                       @mounted="$nextTick(_ => observer.observe($event))" @unmounted="$nextTick(_ => observer.unobserve($event))"
+                       :style="{ marginTop: key === 0 && firstMargin || 0 }" ref="rows">
             </component>
-            <div class="fs-sizer" :style="{ height: sizerHeight }"></div>
-        </div>
-        <div class="fs-scroller" ref="scroller" tabindex="-1">
-            <div class="fs-sizer" :style="{ height: sizerHeight }"></div>
         </div>
     </div>
 </template>
 
 <script>
-    const maxHeight = 10000000;
-    const keys = {
-        "ArrowDown": .05,
-        "ArrowUp": -.05,
-        "PageDown": 1,
-        "PageUp": -1,
-        "Space": 1
-    };
+    const visibleElements = [];
 
-    module.exports = {
+    export default {
         name: "fusion-list",
         props: {
             textField: {
@@ -47,152 +37,99 @@
 
         data() {
             return {
-                index: null,
-                scale: 1,
+                index: 0,
                 content: null,
-                scroller: null,
-                observer: null,
-                firstMargin: "1px",
-                sizerHeight: "1px",
+                observer: null
             };
         },
 
         methods: {
-            _getRows() {
-                return Array.from(this.content.querySelectorAll(".fs-list-row:not(.fs-sizer)"));
-            },
+            instersectionCallback(entries) {
+                entries.reverse().forEach(function(entry) {
+                    if (entry.isIntersecting) {
+                        visibleElements[entry.boundingClientRect.bottom < entry.rootBounds.height ? "unshift" : "push"](entry.target);
+                    } else {
+                        const visibilityIndex = visibleElements.indexOf(entry.target);
 
-            intersectionCallback(entries) {
-                entries.forEach((entry) => {
-                    const component = entry.target.__vue__;
-                    const event = entry.isIntersecting ? "showing" : "hiding";
-
-                    component && component[event] && component[event].call(component);
+                        visibilityIndex >= 0 && visibleElements.splice(visibilityIndex, 1);
+                    }
                 });
-            },
 
-            scrollCallback() {
-                const endOffset = this.source.length - this._getRows().length;
+                if (visibleElements.length) {
+                    this.index = visibleElements[0].__key__ - this.indexOffset;
 
-                let index = Math.round(this.scroller.scrollTop / this.rowHeight * this.scale) - this.indexOffset;
+                    this.index += this.index % this.step;
 
-                index += index % this.step;
-
-                index < 0 && (index = 0);
-                index > endOffset && (index = endOffset);
-
-                this.index = index;
-                this.content.scrollTop = this.scroller.scrollTop;
-            },
-
-            passWheel(e) {
-                if (e.deltaMode) {
-                    const lineHeight = keys["ArrowDown"] * this.content.clientHeight;
-
-                    this.scroller.scrollTop += e.deltaY * lineHeight;
-                    this.scroller.scrollLeft += e.deltaX * lineHeight;
-                } else {
-                    this.scroller.scrollTop += e.deltaY;
-                    this.scroller.scrollLeft += e.deltaX;
+                    this.index < 0 && (this.index = 0);
+                    this.index > this.source.length - this.pageSize - 1 && (this.index = this.source.length - this.pageSize - 1);
                 }
-            },
-
-            passKeys(e) {
-                keys[e.code] && this.scroller.scroll({
-                    top: this.scroller.scrollTop + keys[e.code] * this.content.clientHeight,
-                    behavior: e.repeat ? "instant" : "smooth"
-                });
             }
         },
 
         computed: {
+            firstMargin() {
+                return `${this.index * this.contentHeight}px`;
+            },
+
+            sizerHeight() {
+                return `${this.source.length * this.contentHeight}px`;
+            },
+
+            indexOffset() {
+                return Math.round((this.pageSize - this.content.clientHeight / this.contentHeight) / 2)
+            },
+
+            firstRowRect() {
+                return this.$refs.rows[0].$el.getBoundingClientRect();
+            },
+
+            lastRowRect() {
+                return this.$refs.rows[this.$refs.rows.length - 1].$el.getBoundingClientRect();
+            },
+
+            contentHeight() {
+                if (!this.content) {
+                    return 1;
+                }
+
+                return Math.round((this.lastRowRect.top - this.firstRowRect.top + this.lastRowRect.height) / this.view.length);
+            },
+
             view() {
                 return this.source.slice(this.index, this.index + +this.pageSize);
             }
         },
 
-        watch: {
-            index() {
-                const rows = this._getRows();
-                const len = rows.length;
-
-                if (len) {
-                    const firstRowRect = rows.shift().getBoundingClientRect();
-                    const lastRowRect = len > 1 && rows.pop().getBoundingClientRect();
-
-                    this.rowHeight = Math.round((lastRowRect.top - firstRowRect.top + lastRowRect.height) / len);
-
-                    const sizerHeight = this.source.length * this.rowHeight;
-
-                    this.scale = sizerHeight / maxHeight;
-                    this.scale <= 1 && (this.scale = Math.ceil(this.scale));
-
-                    this.firstMargin = this.index * this.rowHeight / this.scale;
-                    this.visibleCount = Math.ceil(this.content.clientHeight / this.rowHeight);
-                    this.indexOffset = Math.floor((this.pageSize - this.visibleCount) / 2);
-                    this.sizerHeight = `${this.scale <= 1 ? sizerHeight :
-                        Math.max(this.pageSize * this.rowHeight + this.firstMargin, sizerHeight / this.scale)}px`;
-                }
-            }
-        },
-
         mounted() {
             this.content = this.$refs.content;
-            this.scroller = this.$refs.scroller;
-            const passProxy = this.passKeys.bind(this);
 
-            this.content.addEventListener("wheel", this.passWheel.bind(this), true);
-            this.$el.addEventListener("keydown", passProxy);
-
-            this.scroller.addEventListener("scroll", this.scrollCallback.bind(this), { passive: true });
-
-            this.observer = new IntersectionObserver(this.intersectionCallback.bind(this), {
+            this.observer = new IntersectionObserver(this.instersectionCallback.bind(this), {
                 root: this.content,
                 rootMargin: "0px",
                 threshold: 0
             });
-
-            this.index = 0;
-
-            this.$emit("mounted");
         },
     }
 </script>
 
 <style scoped>
-    .fs-list {
+    .ab-list {
         height: 100%;
-        display: flex;
     }
 
-    .fs-list-content,
-    .fs-scroller {
+    .ab-list-content {
         position: relative;
         height: 100%;
         overflow: hidden;
         overflow-y: auto;
-        will-change: contents;
     }
 
-    .fs-list-content {
-        overflow: hidden;
-        width: 100%;
-        top: 0;
-        flex: 1;
-    }
-
-    .fs-scroller {
-        min-width: 17px;
-        flex-basis: fit-content;
-        outline: none;
-    }
-
-    .fs-sizer {
+    .ab-list-sizer {
         position: absolute;
         top: 0;
         left: 0;
         width: 0;
         height: 100%;
-        border-left: 1px solid transparent;
+        border: 1px solid transparent;
     }
 </style>
